93d60324c9f740bb2a5abedc3794cb5595d7cfba
gardenee <snnjjang@naver.com>
2023-03-05 00:32:27 +0900

    feat - 할 일 리스트 껍데기 추가

---
+++ src/components/TodoListItem.js
@@ -0,0 +1,18 @@
+import { FaTrash, FaSquare, FaCheckSquare } from 'react-icons/fa';
+import './TodoListItem.scss';
+
+const TodoListItem = () => {
+  return (
+    <div className="TodoListItem">
+      <div className="checkbox">
+        <FaSquare />
+        <div className="text">할 일!</div>
+      </div>
+      <div className="remove">
+        <FaTrash />
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;
