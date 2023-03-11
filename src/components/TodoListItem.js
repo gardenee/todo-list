9f9e378a52b9a4ff387e21f28fd49fa57460175f
gardenee <snnjjang@naver.com>
2023-03-11 22:31:36 +0900

    feat:토글 기능 구현

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,12 +2,12 @@ import { FaTrash, FaSquare, FaCheckSquare } from 'react-icons/fa';
 import './TodoListItem.scss';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
   const { id, text, checked } = todo;
 
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })}>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <FaCheckSquare /> : <FaSquare />}
         <div className="text">{text}</div>
       </div>
