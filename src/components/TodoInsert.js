c51d8e1d7d4707add9234d853a506f2b1bb5d37c
gardenee <snnjjang@naver.com>
2023-03-11 22:20:21 +0900

    feat: 항목 추가 기능 구현

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,32 @@
 import './TodoInsert.scss';
 import { FaPen } from 'react-icons/fa';
+import { useCallback, useState } from 'react';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="다음의 할 일!" type="text" />
-      <button type="submt">
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="다음의 할 일!"
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+      <button>
         <FaPen />
       </button>
     </form>
